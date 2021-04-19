2eb8afa42d24339bc06bbd177b4c59400c21135b
Warso30 <56986167+Warso30@users.noreply.github.com>
2021-04-18 22:34:43 -0700

    Dijkstra function is fully implemented

--- graph.cc
+++ graph.cc
@@ -1,67 +1,89 @@
+#include <climits>
 #include <iostream>
 #include "graph.h"
+#include "heap.h"
 
-void list_insert(struct list_entry **list, int pos, struct edge edge){
+void list_insert(struct list_entry **list, int vertex, struct edge edge){
+	int pos = vertex - 1;
 	struct list_entry *old_head = list[pos];
-	list_entry[pos] = new list_entry;
-	list_entry[pos]->edge = edge;
-	list_entry[pos]->next = old_head;
+	list[pos] = new list_entry;
+	list[pos]->edge = edge;
+	list[pos]->next = old_head;
 }
 
-struct edge find_min(struct list_entry **graph, int vertex_u, int value){
-	struct list_entry *temp = graph[vertex_u];
-	if (temp == NULL)
-		return NULL;
-	struct edge min = temp->edge;
-	while (temp != null){
-		if (temp->edge.vertex_v != value && temp->edge.vertex_v < min.vertex_v)
-			min = temp->edge;
-		temp = temp->next;
-	}
-	
-}
-
-void Dijkstra(struct vertex *path, struct list_entry **graph, int num_vertices, int source, int destination, int flag){
-	int heap_size = 0;
-	int path_size = 0;
-	struct vertex *heap = new vertex[num_vertices];
-	struct vertex *vertices = new vertex[num_vertices];
-	for (int i = 0; i < num_vertices; i++){
-		vertices[i].id = i;
-		vertices[i].pred = -1;
-		vertices[i].dist = INT_MAX;
-	}
-	vertices[source].dist = 0;
-	heap_insert(heap, vertices[source], heap_size);
-	heap_size++;
-	if (flag == 1)
-		cout << "Insert vertex %d, key=%12.4f\n";
-	for (int i = 0; i < num_vertices  - 1; i++){
-		if (size == 0)
-			break;
-		else{
-			struct vertex vertex_u = extract_min(heap, size);
-			path[i] = vertex_u;
-			path_size++;
-			struct edge min = find_min(graph, vertex_u.id, 0);
-			while (min != NULL){
-				int j;
-				for (j = 0; j < path_size; j++){
-					if (min.vertex_v == path[j].id)
-						break;
-				}
-				if (j == path_size){
-					if (vertex_u.dist + min.weight < vertices[min.vertex_v].dist){
-						vertices[min.vertex_v].dist = vertex_u.dist + min.weight;
-						vertices[min.vertex_v].pred = vetex_u.id;
-						heap_insert(heap, vertices[min.vertex_v], heap_size);
-						heap_size++;
-					}
-				}
-				min = find_min(graph, vertex_u.id, min.vertex_v);
-			}
+void delete_list(struct list_entry **list, int size){
+	for (int i = 0; i < size; i++){
+		struct list_entry *temp = list[i];
+		struct list_entry *curr = temp;
+		while(temp != NULL){
+			temp = temp->next;
+			curr->next = NULL;
+			delete curr;
+			curr = temp;
 		}
 	}
-	
+	delete[] list;
 }
 
+struct edge* find_edge(struct list_entry **graph, int vertex_u, int value){
+	struct list_entry *temp = graph[vertex_u - 1];
+	if (temp == NULL)
+		return NULL;
+	struct edge edge = temp->edge;
+	while (temp != NULL){
+		if (temp->edge.vertex_v > value && temp->edge.vertex_v < edge.vertex_v)
+			edge = temp->edge;
+		temp = temp->next;
+	}
+	if (edge.vertex_v < value)
+		return &edge;
+	else
+		return NULL;
+}
+
+int Dijkstra(struct vertex *vertices, struct list_entry **graph, int num_vertices, int source, int destination, int flag){
+	int heap_size = 0;
+	struct vertex *heap = new vertex[num_vertices];
+	for (int i = 0; i < num_vertices; i++){
+		vertices[i].id = i + 1;
+		vertices[i].pred = -1;
+		vertices[i].dist = INT_MAX;
+		vertices[i].inserted = 0;
+		vertices[i].deleted = 0;
+		vertices[i].determined = 0;
+	}
+	vertices[source].dist = 0;
+	vertices[source].inserted = 1;
+	heap_insert(heap, vertices[source], heap_size, flag);
+	heap_size++;
+	while (heap_size != 0){
+		struct vertex vertex_u = extract_min(heap, heap_size, flag);
+		vertex_u.deleted = 1;
+		vertex_u.determined = 1;
+		heap_size--;
+		vertices[vertex_u.id - 1] = vertex_u;
+		if (vertex_u.id == destination)
+			break;
+		struct edge *adj = find_edge(graph, vertex_u.id, 0);
+		while (adj != NULL){
+			struct vertex vertex_v = vertices[adj->vertex_v - 1];
+			if (vertex_v.determined == 0){
+				if (vertex_u.dist + adj->weight < vertex_v.dist){
+					vertex_v.dist = vertex_u.dist + adj->weight;
+					vertex_v.pred = vertex_u.id;
+					if (vertex_v.inserted == 0){
+						vertex_v.inserted = 1;
+						heap_insert(heap, vertex_v, heap_size, flag);
+						heap_size++;
+					}
+					else
+						decrease_key(heap, vertex_v.id, heap_size, flag);
+					vertices[adj->vertex_v - 1] = vertex_v;
+				}
+			}
+			adj = find_edge(graph, vertex_u.id, adj->vertex_v);
+		}
+	}
+	delete[] heap;
+	return heap_size;
+}
