d1d1510bf04df07e906ca3e8ae92a2096f2b6460
Warso30 <56986167+Warso30@users.noreply.github.com>
2021-04-21 17:41:49 -0700

    Update heap.h

--- heap.h
+++ heap.h
@@ -1,17 +1,16 @@
 struct vertex{
-	int id;
-	int pred;
-	float dist;
-	int inserted;
-	int deleted;
-	int determined;
+	int id;//Name of the vertex
+	int pred;//Predecessor of the vertex
+	float dist;//Current distance from source
+	int inserted;//Used to check if the vertex has been inserted into the min-heap
+	int deleted;//Used to check if the vertex has been deleted from the min-heap
+	int determined;//Used to check if the vertex has been added to the source-destination path set
 };
 
 int left(int);
 int right(int);
 int parent(int);
 void min_heapify(struct vertex*, int, int);
-void build_min_heap(struct vertex*, int);
 void heap_insert(struct vertex*, struct vertex, int, int);
 void decrease_key(struct vertex*, struct vertex, int, int);
 struct vertex extract_min(struct vertex*, int, int);
